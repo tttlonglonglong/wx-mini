0337b17444f486b31fd868d79f25008b75a65032
amazing T <>
2020-09-21 00:12:23 +0800

    first commit

---
+++ 01.wx_mini_prac/pages/index/index.js
@@ -0,0 +1,69 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    
+    userInfo: {},
+    num:'111',
+    time:'',
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../test/test'
+    })
+  },
+  onLoad: function () {
+    // wx.showToast({
+    //   title: '成功-index',
+    //   icon: 'success',
+    //   duration: 8000
+    // })
+    console.log('app.globalData.num',app.globalData.num)
+    this.setData({
+      num:app.globalData.num,
+      time:wx.getStorageSync('time')
+    })
+    if (app.globalData.userInfo) {
+      this.setData({ 
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          num:app.globalData.num,
+          time:wx.getStorageSync('time'),
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
